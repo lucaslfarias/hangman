af44e9452750f3e22fbab8af182149da368b614b
Lucas Liberal <73400397+lucaslfarias@users.noreply.github.com>
2024-06-04 12:14:45 -0300

    hangman game

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # hangman
+
+I am learning and practicing code in python. Just for fun until now.
---
+++ main.py
@@ -0,0 +1,34 @@
+import random
+
+word_list = ["aardvark", "baboon", "camel"]
+chosen_word = random.choice(word_list)
+chosen_word_1 = list(chosen_word)
+chosen_word_1 = ['_' for _ in chosen_word_1]
+erros = 0
+while str(chosen_word_1) != list(chosen_word):
+    guess = input("Guess a letter: ").lower()
+    if guess not in list(chosen_word):
+        print("Try again")
+        erros+=1
+    if erros > 8:
+        print()
+        print("|----- ")
+        print("|    | ")
+        print("|    O ")
+        print("|   /|\\ ")
+        print("|    | ")
+        print("|   / \\ ")
+        print("_      ")
+        print()
+        
+        break
+    else:
+        i=0
+        for i in range (0, len(list(chosen_word))):
+            if guess == list(chosen_word)[i]:
+                chosen_word_1[i] = guess        
+            i+=1
+        print(chosen_word_1)
+                
+    if chosen_word_1 == chosen_word:
+        break
