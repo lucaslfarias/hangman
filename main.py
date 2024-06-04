97e3a5bd4b122cea0f02803baaa21c39498c680c
Lucas Liberal <73400397+lucaslfarias@users.noreply.github.com>
2024-06-04 14:29:18 -0300

    custom

--- main.py
+++ main.py
@@ -1,26 +1,79 @@
 import random
 
+
 word_list = ["aardvark", "baboon", "camel"]
 chosen_word = random.choice(word_list)
 chosen_word_1 = list(chosen_word)
 chosen_word_1 = ['_' for _ in chosen_word_1]
 erros = 0
+
+
 while str(chosen_word_1) != list(chosen_word):
     guess = input("Guess a letter: ").lower()
     if guess not in list(chosen_word):
         print("Try again")
         erros+=1
-    if erros > 8:
+    if erros == 1:
+                print()
+        print("|----- ")
+        print("|    | ")
+        print("|    O ")
+        print("|      ")
+        print("|      ")
+        print("|      ")
+        print("_      ")
+        print()
+    if erros == 2:
+               print()
+        print("|----- ")
+        print("|    | ")
+        print("|    O ")
+        print("|   /  ")
+        print("|       )
+        print("|      ")
+        print("_      ")
+        print()
+    if erros == 3:
+                print()
+        print("|----- ")
+        print("|    | ")
+        print("|    O ")
+        print("|   /| ")
+        print("|    | ")
+        print("|      ")
+        print("_      ")
+        print()
+    if erros == 4:
+               print()
+        print("|----- ")
+        print("|    | ")
+        print("|    O ")
+        print("|   /|\\")
+        print("|    | ")
+        print("|      ")
+        print("_      ")
+        print()
+    if erros == 5:
+         print()
+        print("|----- ")
+        print("|    | ")
+        print("|    O ")
+        print("|   /|\\")
+        print("|    | ")
+        print("|   / ")
+        print("_      ")
+        print()
+    if erros == 6:
         print()
         print("|----- ")
         print("|    | ")
         print("|    O ")
-        print("|   /|\\ ")
+        print("|   /|\\")
         print("|    | ")
-        print("|   / \\ ")
+        print("|   / \\")
         print("_      ")
         print()
-        
+
         break
     else:
         i=0
@@ -31,4 +84,5 @@ while str(chosen_word_1) != list(chosen_word):
         print(chosen_word_1)
                 
     if chosen_word_1 == chosen_word:
+        print(chosen_word_1)
         break
